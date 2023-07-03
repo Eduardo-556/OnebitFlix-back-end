2d3913ad73d66e97db735323a31a8a8590aa9008
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-02 23:59:20 -0300

    Episode resource

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,10 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 Category.hasMany(Course);
-
 Course.belongsTo(Category);
-export { Category, Course };
+Course.hasMany(Episode);
+Episode.belongsTo(Course);
+
+export { Category, Course, Episode };
