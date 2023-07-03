de98a221b8c17dab147dacc244d8603798923a9b
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-03 00:45:08 -0300

    Adicionado upload resource do AdminJS

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,5 +1,6 @@
-import { ResourceOptions } from "adminjs";
-
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 export const episodeResourceOptions: ResourceOptions = {
   navigation: "Catálogo",
   editProperties: [
@@ -31,3 +32,19 @@ export const episodeResourceOptions: ResourceOptions = {
     "updatedAt",
   ],
 };
+
+export const episodeResourceFeature: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, "../../../uploads"),
+      },
+    },
+    properties: {
+      key: "videoUrl",
+      file: "uploadVideo",
+    },
+    uploadPath: (record, filename) =>
+      `videos/course-${record.get("course")}/${filename}`,
+  }),
+];
--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,7 +2,7 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeature, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -16,5 +16,6 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Episode,
     options: episodeResourceOptions,
+    features: episodeResourceFeature,
   },
 ];
