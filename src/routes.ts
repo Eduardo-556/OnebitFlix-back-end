0c047112c733d3b86fca1c5b84f1c9f79416057b
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-06 15:04:53 -0300

    incluindo rotas, categories

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import { categoriesController } from "./controllers/categoriesController";
+const router = express.Router();
+router.get("/categories", categoriesController.index);
+
+export { router };
