f214f0aa9fb2c94df3ce1bdd051bcaccfc33a23a
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-06 16:21:31 -0300

    Obtendo cursos de uma categoria

--- src/routes.ts
+++ src/routes.ts
@@ -2,5 +2,6 @@ import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
 const router = express.Router();
 router.get("/categories", categoriesController.index);
+router.get("/categories/:id", categoriesController.show);
 
 export { router };
