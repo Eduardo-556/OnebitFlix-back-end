347074efbc955b1426277124b5654187a30a0d47
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-10 16:55:10 -0300

    Protegendo os vídeos

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -34,3 +34,34 @@ export function ensureAuth(
     });
   });
 }
+
+export function ensureAuthviaQuery(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const { token } = req.query;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Não autorizado: nenhum token encontrado." });
+  }
+  if (typeof token !== "string") {
+    return res
+      .status(400)
+      .json({ message: "O parâmetro token deve ser do tipo string." });
+  }
+
+  jwtService.verifyToken(token, (err, decoded) => {
+    if (err || typeof decoded === "undefined") {
+      return res
+        .status(401)
+        .json({ message: "Não autorizado: token inválido." });
+    }
+    userService.findByEmail((decoded as JwtPayload).email).then((user) => {
+      req.user = user;
+      next();
+    });
+  });
+}
