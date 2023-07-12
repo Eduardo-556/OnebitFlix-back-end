d0e9977cd2c197799283f6cb6b31510abea2dad6
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-12 17:31:00 -0300

    adicionado top 10 cursos populares

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,7 @@ router.get("/categories/:id", ensureAuth, categoriesController.show);
 router.get("/courses/featured", ensureAuth, coursesController.featured);
 router.get("/courses/newest", coursesController.newest);
 router.get("/courses/search", ensureAuth, coursesController.search);
+router.get("/courses/popular", ensureAuth, coursesController.popular);
 router.get("/courses/:id", ensureAuth, coursesController.show);
 router.get("/episodes/stream", ensureAuthviaQuery, episodesController.stream);
 router.get("/favorites", ensureAuth, favoriteController.index);
